bc5fe46825854f71a5d4e1ee07ede5123755ba5c
Danyl Timofeyev <timofeev.danil@gmail.com>
2020-12-21 13:55:48 +0200

    Initial Commit

---
+++ MyCoordinatorsTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCoordinatorsTemplate
+//
+//  Created by Danyl Timofeyev on 21.12.2020.
+//  Copyright © 2020 Danyl Timofeyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
